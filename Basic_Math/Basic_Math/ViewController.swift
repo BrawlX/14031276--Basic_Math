04a86e53a03e1cc513e9acd730017787d8e8fb87
BrawlX <brawlx13@yahoo.co.uk>
2017-02-21 12:15:25 +0000

    Add files via upload

--- Basic_Math/Basic_Math/ViewController.swift
+++ Basic_Math/Basic_Math/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var answer = 0
+    var rng1 = Int(arc4random_uniform(5))
+    var rng2 = Int(arc4random_uniform(5))
+    var sum = 0
+    
+    @IBOutlet weak var math: UILabel!
+    
+    
+    
+    @IBOutlet weak var congratsView: UIView!
+    
+    
+    
+    @IBAction func ans(sender: AnyObject) {
+        answer = sender.tag
+        if answer == sum{
+            math.text = "Correct"
+        }
+        else{
+            math.text = "Wrong"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        rng1 = Int(arc4random_uniform(5))
+        rng2 = Int(arc4random_uniform(5))
+        sum = rng1 + rng2
+        math.text = String(rng1) + " + " + String(rng2) + " = ?"
+        
     }
 
     override func didReceiveMemoryWarning() {
