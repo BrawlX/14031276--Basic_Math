68bc39e59336ab934b1f9f087914d4b4b1f87a03
BrawlX <brawlx13@yahoo.co.uk>
2017-02-23 11:06:06 +0000

    Add files via upload

--- Basic_Math/Basic_Math/ViewController.swift
+++ Basic_Math/Basic_Math/ViewController.swift
@@ -21,14 +21,25 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var correct: UILabel!
     
+    @IBOutlet weak var stars: UIView!
+    
+    @IBOutlet weak var wrong: UILabel!
+    
+    @IBOutlet weak var correct2: UILabel!
+    
+    
     @IBAction func ans(sender: AnyObject) {
         answer = sender.tag
         if answer == sum{
             congratsView.hidden = false
-            correct.text = String(rng1) + " + " + String(rng2) + " = " + String(sum)
+            correct.text = String(rng1) + " + " + String(rng2) + " = "
+            correct2.text = String(sum)
+            UIView.animateWithDuration(0.5, delay: 0, options: [.Autoreverse, .Repeat], animations: {self.stars.alpha = 0}, completion: nil)
         }
         else{
-            math.text = "Wrong"
+            UIView.animateWithDuration(1, animations:{ self.wrong.alpha = 1
+                self.wrong.alpha = 0
+                },completion: nil)
         }
     }
     
@@ -39,11 +50,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        wrong.alpha = 0
         congratsView.hidden = true
         rng1 = Int(arc4random_uniform(5))
         rng2 = Int(arc4random_uniform(5))
         sum = rng1 + rng2
-        math.text = String(rng1) + " + " + String(rng2) + " = ?"
+        math.text = String(rng1) + " + " + String(rng2) + " ="
     }
 
     override func didReceiveMemoryWarning() {
